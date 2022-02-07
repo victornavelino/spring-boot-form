9d5aabf6c99ed6b04a2eeee3c9d627570dd2f436
victornavelino <victornavelino@gmail.com>
2022-02-07 12:50:44 -0300

    validaciones en el controlador

--- src/main/java/com/springboot/form/controllers/FormularioController.java
+++ src/main/java/com/springboot/form/controllers/FormularioController.java
@@ -1,5 +1,8 @@
 package com.springboot.form.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -24,6 +27,11 @@ public class FormularioController {
 		model.addAttribute("titulo", "Resultado Form");
 		
 		if (result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err ->{
+				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
 			return "form";
 		}
 
