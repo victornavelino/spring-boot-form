50d171527a984f275f9fe190676fc91eea8ee4fb
victornavelino <victornavelino@gmail.com>
2022-02-09 12:56:30 -0300

    validaciones automaticas

--- src/main/java/com/springboot/form/controllers/FormularioController.java
+++ src/main/java/com/springboot/form/controllers/FormularioController.java
@@ -1,7 +1,7 @@
 package com.springboot.form.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.springboot.form.models.entidades.Usuario;
 
@@ -29,11 +30,11 @@ public class FormularioController {
 		model.addAttribute("titulo", "Resultado Form");
 		
 		if (result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
+//			Map<String, String> errores = new HashMap<>();
+//			result.getFieldErrors().forEach(err ->{
+//				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+//			});
+//			model.addAttribute("error", errores);
 			return "form";
 		}
 
