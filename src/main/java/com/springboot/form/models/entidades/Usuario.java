427a5b27c13e3e25d1346ab4f717a6bb81565429
victornavelino <victornavelino@gmail.com>
2022-02-11 07:33:42 -0300

    sdsd

--- src/main/java/com/springboot/form/models/entidades/Usuario.java
+++ src/main/java/com/springboot/form/models/entidades/Usuario.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.Size;
 
 public class Usuario {
 	
-	@Pattern(regexp = "[0-9]{2}[.][0-9]{3}")
+	@Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
 	private String identificador;
 	
 	@NotEmpty
